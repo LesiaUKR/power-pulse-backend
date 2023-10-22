3f95d39088687fb71923d1724cc94a1b9da24079
Serhii Ozeran <ozeransm@gmail.com>
2023-10-22 23:14:08 +0300

    update

--- controllers/auth/singin.js
+++ controllers/auth/singin.js
@@ -1,35 +1,39 @@
-const Users = require("../../models/users");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET_KEY } = process.env;
 const bcrypt = require('bcryptjs');
+const Users = require("../../models/users");
+const createError = require("../../helpers/createError");
 
 const singin = async (req, res, next) => {
     const { email, password } = req.body;
     const user = await Users.findOne({ email });
-    const result = await bcrypt.compare(password, user.password);
-    if (result)
-        if (!user.token) {
-            const token = jwt.sign({_id: user._id}, JWT_SECRET_KEY, { expiresIn: '1d' });
-            const updateUser = await Users.findByIdAndUpdate(user._id, { token });
-            res.status(200);
-            res.json({
-                message: "login success",
-                token
-            })
-        } else {
-            res.status(200);
-            res.json({
-                token: user.token,
-                user:{
-                    email: user.email,
-                    
-                }
-            })
-        }
-    else {
-        next(createError('UNAUTHORIZED', 'Email or password is wrong'));
-    }
+    console.log(user);
+    if (user) {
+        const result = await bcrypt.compare(password, user.password);
     
+        if (result)
+            if (!user.token) {
+                const token = jwt.sign({ _id: user._id }, JWT_SECRET_KEY, { expiresIn: '1d' });
+                const updateUser = await Users.findByIdAndUpdate(user._id, { token });
+                res.status(200);
+                res.json({
+                    message: "login success",
+                    token
+                })
+            } else {
+                res.status(200);
+                res.json({
+                    token: user.token,
+                    user: {
+                        email: user.email,
+                    
+                    }
+                })
+            }
+        else {
+            next(createError('UNAUTHORIZED', 'Email or password is wrong'));
+        }
+    }else next(createError('UNAUTHORIZED', 'Email or password is wrong'));
 }
 
 module.exports = singin;
