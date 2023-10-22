8b8ebf14c288cbdc76c984ef11841576508a1f32
Your Name <solovyova.lesya@gmail.com>
2023-10-23 00:12:03 +0300

    deleted data and middleware doubled folder

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const current = require('../../controllers/current');
-const singup = require('../../controllers/auth/singup');
-const singin = require('../../controllers/auth/singin');
-const logout = require('../../controllers/auth/logout');
-const refresh = require('../../controllers/auth/refresh');
-const dailyMetrics = require('../../controllers/auth/dailyMetrics')
-const auth = require('../../midlewares/auth');
+const express = require("express");
+const current = require("../../controllers/current");
+const singup = require("../../controllers/auth/singup");
+const singin = require("../../controllers/auth/singin");
+const logout = require("../../controllers/auth/logout");
+const refresh = require("../../controllers/auth/refresh");
+const dailyMetrics = require("../../controllers/auth/dailyMetrics");
+const auth = require("../../middlewares/auth");
 const router = express.Router();
 
-router.post('/users/register', singup);
-router.post('/users/login', singin);
-router.post('/users/logout',auth , logout);
-router.get('/users', current);
-router.post('/users/verify', refresh);
+router.post("/users/register", singup);
+router.post("/users/login", singin);
+router.post("/users/logout", auth, logout);
+router.get("/users", current);
+router.post("/users/verify", refresh);
 //private
-router.patch('/users/dailyMetrics', dailyMetrics)
+router.patch("/users/dailyMetrics", dailyMetrics);
 // router.patch('/users/avatars', auth, upload.single("avatar"), handlerJpg, editAvatar);
 // router.get('/users/verify/:verificationToken', verifyCode);
 module.exports = router;
