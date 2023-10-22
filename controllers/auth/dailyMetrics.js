cbab5399842cae78f41b4344167035a0947ceadc
fxxwol <protsivroksolana2004@gmail.com>
2023-10-22 15:58:21 +0300

    schemas and calculate bmr

---
+++ controllers/auth/dailyMetrics.js
@@ -0,0 +1,9 @@
+const getBMR = async (req, res, next) => {
+    try {
+        console.log('BMR')
+
+    } catch (error) {
+
+    }
+}
+module.exports = getBMR
---
+++ helpers/calculateBMR.js
@@ -0,0 +1,19 @@
+const calculateBMR = (currentWeight, height, birthday, levelActivity, sex) => {
+    const activityLevelFactor = {
+        1: 1.2,
+        2: 1.375,
+        3: 1.55,
+        4: 1.725,
+        5: 1.9,
+    };
+    let BMR = 0;
+    const age = new Date().getFullYear() - birthday.getFullYear();
+    if (sex === 'female') {
+        BMR = (10 * currentWeight + 6, 25 * height - 5 * age - 161) * activityLevelFactor[levelActivity]
+    } else {
+        BMR = (10 * currentWeight + 6, 25 * height - 5 * age + 5) * activityLevelFactor[levelActivity]
+    }
+    return BMR;
+}
+
+module.exports = calculateBMR;
