c57c056e42f6b03aa1e50953790ac8c419467f69
Vikki2Rikki <119513480+Vikki2Rikki@users.noreply.github.com>
2023-10-26 16:47:05 +0300

    add products in swagger

--- App.js
+++ App.js
@@ -3,15 +3,15 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-
-const loginRouter = require('./routes/auth/auth');
+const loginRouter = require("./routes/auth/auth");
 const productsRouter = require("./routes/products/products");
-const exercisesRouter = require('./routes/exercises')
+const exercisesRouter = require("./routes/exercises");
 
 const app = express();
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -20,9 +20,9 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', loginRouter);
+app.use("/api", loginRouter);
 app.use("/api/products", productsRouter);
-app.use('/api/exercises', exercisesRouter)
+app.use("/api/exercises", exercisesRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
