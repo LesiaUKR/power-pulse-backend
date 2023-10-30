6edc56e27f2c1579254e7bc3ac5ed3f559f92c58
Serhii Ozeran <ozeransm@gmail.com>
2023-10-30 15:11:39 +0200

    update schemas

--- models/schemaRegister.js
+++ models/schemaRegister.js
@@ -3,12 +3,22 @@ const EMAIL_REGEX = require('../helpers/constants');
 
 const schemaRegister = Joi.object({
     name: Joi.string()
-        .min(2).required(),
+        .min(2)
+        .max(15)
+        .required(),
     email: Joi.string()
         .pattern(EMAIL_REGEX)
-        .required(),
+        .required()
+        .messages({
+            'string.base': 'The email must be a string.',
+            'any.required': 'The email field is required.',
+            'string.empty': 'The email must not be empty.',
+            'string.pattern.base': 'The email must be in format test@test.com.'
+        }),
+    password: Joi.string()
+        .min(6)
+        .required()
         
-    password: Joi.string().min(6).required()
 });
 
 module.exports = schemaRegister;
